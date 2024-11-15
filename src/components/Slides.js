c6819f2a0f550138cc2750607fb3b409fd01b827
Juan Ortiz <jay@Juans-MacBook-Pro.local>
2024-11-15 18:56:23 -0400

    Eliminated slides

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -6,16 +6,6 @@ const slides = [
     description: 'A champion in the ring and in life.',
     imageUrl: 'https://www.thetimes.com/imageserver/image/%2Fmethode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fec5e0671-d12f-48b8-864e-f27e730f96a6.jpg?crop=1600%2C900%2C0%2C0&resize=1500',
   },
-  {
-    title: '',
-    description: '',
-    imageUrl: 'https://talksport.com/wp-content/uploads/sites/5/2024/08/GettyImages-2166636569.jpg?strip=all&w=960',
-  },
-  {
-    title: '',
-    description: '',
-    imageUrl: 'https://ringside24.com/media/post/jake-paul-vs-mike-tyson-undercard_slug.jpg',
-  },
 ];
 
 const Slides = () => {
